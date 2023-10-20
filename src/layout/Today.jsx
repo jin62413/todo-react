685ca3f0752464f3ae453f40a3c4c06402b6b0e8
jin62413 <jin62413@gmail.com>
2023-10-20 13:49:52 +0900

    feat : 기본 틀 생성, 시계 기능 추가

---
+++ src/layout/Today.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+function Today() {
+  const [today, setToday] = useState('0000년 00월 00일 0요일');
+
+  useEffect(() => {
+    let time = new Date();
+    const week = ['일', '월', '화', '수', '목', '금', '토'];
+
+    setToday(
+      time.getFullYear() +
+        '년 ' +
+        (time.getMonth() + 1) +
+        '월 ' +
+        time.getDate() +
+        '일 ' +
+        week[time.getDay()] +
+        '요일'
+    );
+  }, []);
+
+  return (
+    <div>
+      <p>{today}</p>
+    </div>
+  );
+}
+
+export default Today;
